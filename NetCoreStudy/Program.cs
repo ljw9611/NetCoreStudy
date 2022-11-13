d6a7542ae3f643d2ba1f45fe152f7ce813e91b14
Garden <66628342+ljw9611@users.noreply.github.com>
2022-11-14 03:38:53 +0900

    StatusCode 확인

--- NetCoreStudy/Program.cs
+++ NetCoreStudy/Program.cs
@@ -3,6 +3,12 @@ using NET_Core_Study; // 생성한 미들웨어 namespace using
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    await next();
+    await context.Response.WriteAsync($"\n Status code : { context.Response.StatusCode }");
+});
+
 app.UseMiddleware<Middleware>(); // 미들웨어 클래스 추가 / Middleware는 클래스명
 
 app.MapGet("/", () => "Hello World!12");
